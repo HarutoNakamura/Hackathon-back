fc2ada5943fc00c77be309a53f889f53c64a7286
HarutoNakamura <nahanaha328@gmail.com>
2024-12-03 15:29:29 +0900

    delcomment

--- hackathon/main.go
+++ hackathon/main.go
@@ -15,11 +15,6 @@ import (
 
 var db *sql.DB
 
-type Comment struct {
-	Email   string `json:"email"`
-	Comment string `json:"comment"`
-}
-
 func main() {
 	// TLS証明書の設定
 	rootCert := "./server-ca.pem"
@@ -40,8 +35,6 @@ func main() {
 	defer db.Close()
 
 	// ルーティング
-	http.HandleFunc("/api/comments/post", corsMiddleware(postCommentHandler))
-	http.HandleFunc("/api/comments/get", corsMiddleware(getCommentsHandler))
 	http.HandleFunc("/api/posts/create", corsMiddleware(postHandler))
 	http.HandleFunc("/api/posts/get", corsMiddleware(getPostsHandler))
 	http.HandleFunc("/api/replies/create", corsMiddleware(replyHandler))
@@ -50,7 +43,6 @@ func main() {
 	http.HandleFunc("/api/likes/get", corsMiddleware(getLikesHandler))
 
 	log.Println("Backend server is running on port 8081")
-	//log.Println(db.Query("SHOW TABLES"))
 	http.ListenAndServe(":8081", nil)
 }
 
@@ -241,60 +233,6 @@ func getLikesHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]int{"like_count": likeCount})
-}
-
-// コメント投稿処理
-func postCommentHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-		return
-	}
-
-	var comment Comment
-	if err := json.NewDecoder(r.Body).Decode(&comment); err != nil {
-		http.Error(w, "Invalid input", http.StatusBadRequest)
-		return
-	}
-
-	_, err := db.Exec("INSERT INTO comments (email, comment) VALUES (?, ?)", comment.Email, comment.Comment)
-	if err != nil {
-		log.Printf("Database error: %v", err) // Log database error for debugging
-		http.Error(w, "Database error", http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte("Comment added"))
-}
-
-// コメント取得処理
-func getCommentsHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-		return
-	}
-
-	rows, err := db.Query("SELECT email, comment FROM comments ORDER BY id DESC")
-	if err != nil {
-		log.Printf("Database error: %v", err) // Log database error for debugging
-		http.Error(w, "Database error", http.StatusInternalServerError)
-		return
-	}
-	defer rows.Close()
-
-	var comments []Comment
-	for rows.Next() {
-		var comment Comment
-		if err := rows.Scan(&comment.Email, &comment.Comment); err != nil {
-			log.Printf("Row scan error: %v", err) // Log row scan error
-			http.Error(w, "Row scan error", http.StatusInternalServerError)
-			return
-		}
-		comments = append(comments, comment)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(comments)
 }
 
 // CORSミドルウェア
