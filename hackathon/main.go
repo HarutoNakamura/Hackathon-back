7246d27172ace64a4ae52546d7a41f7bd1f138e5
HarutoNakamura <nahanaha328@gmail.com>
2024-12-04 20:56:34 +0900

    SSL

--- hackathon/main.go
+++ hackathon/main.go
@@ -29,7 +29,7 @@ const (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Print("Error loading .env file")
 	}
 
 	dbUser := os.Getenv("MYSQL_USER")
